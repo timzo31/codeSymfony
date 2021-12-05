21cbc1d998487af60f23a24dc75c7387d51cc347
timzo31 <69592997+timzo31@users.noreply.github.com>
2021-12-06 01:03:45 +0800

    Service Objects

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,19 +6,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
 
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
+        #$html = $twigEnvironment->render('question/homepage.html.twig');
+        #return new Response($html);
+
         return $this->render('question/homepage.html.twig');
     }
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}", name="app_question_show")
      */
     public function show($slug)
     {
@@ -27,7 +31,9 @@ class QuestionController extends AbstractController
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
+
         dump($this);
+
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
