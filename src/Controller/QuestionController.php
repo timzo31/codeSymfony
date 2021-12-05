ff8dda4ac4d0805d099f902798371a27d47aeb53
timzo31 <69592997+timzo31@users.noreply.github.com>
2021-12-05 16:02:14 +0800

    Profiler: the debugging tool

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -27,7 +27,7 @@ class QuestionController extends AbstractController
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-
+        dump($this);
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
